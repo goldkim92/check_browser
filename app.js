a577d33233f7a59e160091e1cfab63803ade9a0a
goldkim92 <goldkim92@gmail.com>
2017-02-21 16:31:08 +0900

    show information just for true

--- app.js
+++ app.js
@@ -10,10 +10,16 @@ app.use(useragent.express());
 
 app.get("/",function(req,res) {
 	var source = req.headers['user-agent'];
-	var output = JSON.stringify(useragent.parse(source));
+	//var output = JSON.stringify(useragent.parse(source));
 	//res.send("<h1>Hello from EC2</h1>");
-	res.send(output);
+	var output = useragent.parse(source);
+	var json_res={};
+	Object.keys(output).forEach(function(key,index) {
+		if(output[key] !== false) {
+			json_res[key] = output[key];
+		}
+	});
+	res.send(json_res);
 });
 
 app.listen(80);
-
